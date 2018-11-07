b8604a486fd37b74707f1aa49d1160a273575e38
David Chambers <dlchambersjr@gmail.com>
2018-11-06 16:44:26 -0800

    Ready for deployment

--- src/chicken/model.js
+++ src/chicken/model.js
@@ -1,14 +1,13 @@
 import mongoose, { Schema } from 'mongoose';
 
 const schema = new Schema({
-  name: { type: String, required: true, unique: true},
+  name: { type: String, required: true, unique: true },
   coop: { type: Schema.Types.ObjectId, ref: 'Coop' },
 });
 
-schema.pre('findOne', function(next) {
-  this.populate('coops');
+schema.pre('findOne', function (next) {
+  this.populate('coop');
   next();
 });
-
 
 export default mongoose.model('Chicken', schema);
